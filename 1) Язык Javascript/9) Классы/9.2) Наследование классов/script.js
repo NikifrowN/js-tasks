adf1e5f1358a5c908384800f120c289b72d64a01
NikiforowN <kiwiwiwitsk@gmail.com>
2022-06-22 01:21:39 +0700

    1.9.1 done

---
+++ 1) Язык Javascript/9) Классы/9.2) Наследование классов/script.js
@@ -0,0 +1,18 @@
+// Ошибка создания экземпляра класса
+
+class Animal {
+   constructor(name) {
+      this.name = name;
+   }
+}
+
+class Rabbit extends Animal {
+   constructor(name) {
+      super(name);
+      this.created = Date.now();
+   }
+}
+
+const rabbit = new Rabbit("Белый кролик");
+
+console.log(rabbit.name);
