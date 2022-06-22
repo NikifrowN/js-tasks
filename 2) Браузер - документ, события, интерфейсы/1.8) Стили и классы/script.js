8e0c5c4fffbfa212ca170202746b22f26adf77c4
NikiforowN <kiwiwiwitsk@gmail.com>
2022-06-22 20:01:08 +0700

    2.1.8 done

---
+++ 2) Браузер - документ, события, интерфейсы/1.8) Стили и классы/script.js
@@ -0,0 +1,18 @@
+function showNotification({top = 0, right = 0, html}) {
+   let elem = document.createElement('div');
+
+   elem.style.top = top + 'px';
+   elem.style.right = right + 'px';
+
+   elem.innerHTML = html;
+   document.body.append(elem);
+
+   setTimeout(() => elem.remove(), 1500);
+}
+
+
+showNotification({
+   top: 10,
+   right: 10,
+   html: 'Hello',
+});
